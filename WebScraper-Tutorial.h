d5af0bf555dd276e44c339362bc0d55fb2493da5
Charles Chae <charleschae12@gmail.com>
2022-10-24 23:26:07 -0400

    Update WebScraper-Tutorial.h
    added the webtitle class

--- WebScraper-Tutorial.h
+++ WebScraper-Tutorial.h
@@ -10,6 +10,96 @@
 #include <curl/curl.h>
 #include <libxml/HTMLparser.h>
 
+
+class WebTitle
+{
+public:
+
+    struct Context
+    {
+      Context(): addTitle(false), dataExtracted(false) { }
+
+      std::string tag;
+      char **attr;
+      bool addTitle;
+      bool dataExtracted;
+      std::string title;
+    };
+
+    WebTitle() {}
+    WebTitle( const std::string &tag, const char ** attr);
+    void parseHtml( );
+    Context& getContext() { return  context;}
+     bool createConn(const std::string &name);
+
+private:
+
+    bool initCurl(const char *url);
+    static int writer(char *data, size_t size, size_t nmemb, std::string *writerData);
+    static void StartElement(void *voidContext,
+                             const xmlChar *name,
+                             const xmlChar **attributes);
+
+
+    static void  EndElement(void *voidContext,
+                           const xmlChar *name);
+    static void  cdata(void *voidContext,
+                      const xmlChar *chars,
+                      int length);
+
+    static void  Characters(void *voidContext,
+                           const xmlChar *chars,
+                           int length);
+
+    static void handleCharacters(Context *context,
+                                 const xmlChar *chars,
+                                 int length);
+    void clearContext(void) ;
+
+    Context context;
+
+    CURL *conn{};
+    CURLcode code;
+    char errorBuffer[CURL_ERROR_SIZE]{};
+    std::string htmlData;
+
+    htmlSAXHandler saxHandler =
+    {
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        StartElement,
+        EndElement,
+        nullptr,
+        Characters,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        cdata,
+        nullptr,
+        0,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+    };
+};
+
 class Scraper
 {
 public:
