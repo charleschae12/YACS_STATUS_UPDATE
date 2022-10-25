befddcafb7019076b751dbf396870e37f1e65533
Charles Chae <charleschae12@gmail.com>
2022-10-24 23:31:15 -0400

    Update WebScraper-Tutorial.cpp
    added initCurl function

--- WebScraper-Tutorial.cpp
+++ WebScraper-Tutorial.cpp
@@ -142,3 +142,49 @@ Scraper::~Scraper()
 //    for( auto& it : threadIds)
 //        it.join();
 }
+
+bool WebTitle::initCurl(const char *url)
+{
+    CURLcode code;
+
+    conn = curl_easy_init();
+
+    if(conn == nullptr) {
+      fprintf(stderr, "Failed to create CURL connection\n");
+      exit(EXIT_FAILURE);
+    }
+
+    code = curl_easy_setopt(conn, CURLOPT_ERRORBUFFER, errorBuffer);
+    if(code != CURLE_OK) {
+      fprintf(stderr, "Failed to set error buffer [%d]\n", code);
+      return false;
+    }
+
+    code = curl_easy_setopt(conn, CURLOPT_URL, url);
+    if(code != CURLE_OK) {
+      fprintf(stderr, "Failed to set URL [%s]\n", errorBuffer);
+      return false;
+    }
+
+    code = curl_easy_setopt(conn, CURLOPT_FOLLOWLOCATION, 1L);
+    if(code != CURLE_OK) {
+      fprintf(stderr, "Failed to set redirect option [%s]\n", errorBuffer);
+      return false;
+    }
+
+    code = curl_easy_setopt(conn, CURLOPT_WRITEFUNCTION, writer);
+    if(code != CURLE_OK) {
+      fprintf(stderr, "Failed to set writer [%s]\n", errorBuffer);
+      return false;
+    }
+
+    htmlData = "";
+    code = curl_easy_setopt(conn, CURLOPT_WRITEDATA, &htmlData);
+    if(code != CURLE_OK) {
+      fprintf(stderr, "Failed to set write data [%s]\n", errorBuffer);
+      return false;
+    }
+
+    return true;
+}
+
