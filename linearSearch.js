e1ace123a607ff1ba5d32bd4d6832d733dd65e9f
Charles Chae <charleschae12@gmail.com>
2022-11-14 16:08:44 -0500

    Update linearSearch.js
    Bubble sort function _ tutorial

--- linearSearch.js
+++ linearSearch.js
@@ -33,3 +33,16 @@ console.log(binarySearch([1, 2, 3, 4], 1)) // 1
 console.log(binarySearch([1, 2, 3, 5, 9], 4)) // -1
 console.log(binarySearch([1, 2, 3, 4, 5], 5)) // 4
 console.log(binarySearch([0, 3], 3)) // 1
+
+function bubbleSort(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // SWAP
+        ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+      }
+    }
+  }
+  return arr
+}
+console.log(bubbleSort([2, 3, 1, 2])) // [1, 2, 2, 3]
