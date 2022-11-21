4f83d5d816c350048b836d7121bc942d96bda9f7
Charles Chae <charleschae12@gmail.com>
2022-11-21 16:55:39 -0500

    Create Sidebar_close_function_template.vue

---
+++ Sidebar_close_function_template.vue
@@ -0,0 +1,28 @@
+<template>
+<div id="app">
+    <ejs-sidebar id="default-sidebar" ref="sidebar" :open="open" :close="close" :showBackdrop="showBackdrop">
+<div class="title"> Sidebar content</div>
+<div class="sub-title">
+    Click the button to close the Sidebar.
+</div>
+<div class="center-align">
+    <button ejs-button id="close" v-on:click="closeClick" class="e-btn close-btn">Close Sidebar</button>
+</div>
+</ejs-sidebar>
+<div>
+<div>
+    <div class="title">Main content</div>
+    <div class="sub-title"> Click the button to Open the Sidebar.
+        <div style="padding:20px" class="center-align">
+            <button ejs-button id="open" class="e-btn e-info" v-on:click="openClick">Open Sidebar</button>
+        </div>
+
+    </div>
+    <div class="sub-title"> Click the button to open/close the Sidebar.
+        <div style="padding:20px" class="center-align">
+            <button ejs-button id="toggle" class="e-btn e-info" v-on:click="toggleClick">Toggle Sidebar</button>
+        </div>
+
+    </div>
+</div>
+</div>
