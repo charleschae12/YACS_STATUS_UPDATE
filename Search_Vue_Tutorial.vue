ddcecd4aaaeaada1673b1ab8393e47f316f7e87f
Charles Chae <charleschae12@gmail.com>
2023-02-17 21:10:02 -0500

    Create Search_Vue_Tutorial.vue
    Search bar Vue tutorial

---
+++ Search_Vue_Tutorial.vue
@@ -0,0 +1,9 @@
+<template>
+   <input type="text" v-model="input" placeholder="Search fruits..." />
+  <div class="item fruit" v-for="fruit in filteredList()" :key="fruit">
+    <p>{{ fruit }}</p>
+  </div>
+  <div class="item error" v-if="input&&!filteredList().length">
+     <p>No results found!</p>
+  </div>
+</template>
